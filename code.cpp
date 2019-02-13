08250a54341ff48091ede8cdd32e195d33531e5c
S-Ramdath <43352059+S-Ramdath@users.noreply.github.com>
2019-02-13 03:07:24 -0400

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+int main() {
+	std::string word;
+	std::ofstream dataout;
+	
+	dataout.open("data.txt");
+	//std::cin >> word;
+	word = "fiewurah9p7werghpa7w9tgfbq3p8e7tgfq3hp947tgq3p97etg08q3t4t0967rw097tyw497rtywh0497ty49towertg4whepthw459e7tn4er7895ben5r87tye4586907u45yhge589ryhje589yet98rygher869yh56eyyh5gh47y85g2t5h5h88t5h5g4t4h4h456h4ry4td86h4d6t8h4grd3h4d6r8y4h68df4hd354th384hrt38f4yh6d8rt4dh6rd8th46dr8ty46dr8th46d8f7hf5gh745gfh4r6e84th3fd4h6rt84fyhrg8fh4rtf64h6rth46df4h65f4h65df4h6fd85gh4fd68h4ft68h74yr68f7h6rt8f4hf5g4h6df5gh41f6g54h6drf5h4r6e8y7h68tf7hf6d5g4h6df5gh4f65gh4fd3h54fd3h54rd6fghf52h46d3fh216f35gh4f6gh58768768f4g6h87dfh54r8f6gh7f6g3fh54gf6h534fg68h7rt687h6fg5h4ft65h74f6g5h74f6t7h4f6g5h34f65gh4f65g3h4fd36hh4w9t7y349075yhtoiueghkfldghskdghdfklgjdshgipfoershviuerg687684f6g54re68t4gerw638g4s3et438e7y43r84g3st4hg3es4gf3e8t4h3j84sh3tds4h384esth3s4h38sd4h38ts4h3sd4ty3seh4st3dh43sdth43dth43tsdh43stdh43tds4h3ds4h3sdt4h3d84th3dst4h3sdth48t7y6e45ts31get32g1s3h4e6t7y86et7y8ew67ryt6e854ht35h4ry68j465ohurpob5rp8yohtjh98trhn9td8phntoudnhdhty587d6r8h46rt8d4uh38tf7h3trd7y3drt57u3dr7tu3dr5yfj4hd3f5h4df3tu7yy3dtr57jdf3tu7h3dty73d5tru7dft5u37drt53u5668j4";
+	std::string output = word;
+	
+	//long long int count[1000000000000000000];
+	long long int maxCount = 10000000000000000000;
+//	for (int i = 0; i < 101; i++) {
+//		count[i] = 0;
+	//}
+	std::cout << "Don't worry, your RAM won't be filled, but your hard disk might, depending on how long you run this :)\n NOTE: It seems that the write speed is only limited by the maximum write speed of your hard disk." << std::endl;
+	system("pause");
+
+	while (true) {
+		while (true) {
+			while (true) {
+				while (true) {
+					while (true) {
+						while (true) {
+							while (true) {
+								while (true) {
+                  while (true) {
+                  dataout << output;
+                  }
+                }
+						  }
+            }
+				  }
+			  }
+		  }
+	  }
+  }
+	
+	dataout.close();
+	system("pause");
+	return 0;
+}
